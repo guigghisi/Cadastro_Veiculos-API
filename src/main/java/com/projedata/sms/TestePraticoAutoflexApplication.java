4a6f8ebd5ded696348abbef47024c69bc36c3a2f
Guigghisi <guilhermeghisi14@gmail.com>
2023-02-01 09:19:06 -0300

    First commit

---
+++ src/main/java/com/projedata/sms/TestePraticoAutoflexApplication.java
@@ -0,0 +1,13 @@
+package com.projedata.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestePraticoAutoflexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestePraticoAutoflexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projedata/sms/TestePraticoAutoflexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projedata.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestePraticoAutoflexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
